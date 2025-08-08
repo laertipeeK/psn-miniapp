446173882e954358f7c7cbeacea8e48969d42c8e
laertipeeK <40959403+laertipeeK@users.noreply.github.com>
2025-08-08 19:59:31 +0300

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import MiniAppStore from './MiniAppStore';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <MiniAppStore />
-  </React.StrictMode>
-);
