d4d551961f8a829db2a349221645a14c7e75f8e8
laertipeeK <40959403+laertipeeK@users.noreply.github.com>
2025-08-08 19:39:19 +0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MiniAppStore from './MiniAppStore';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MiniAppStore />
+  </React.StrictMode>
+);
